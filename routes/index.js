b102137f15eeff4b2f7150a94ef19905cb6eddc4
lolo298 <lorenzoaoulini@gmail.com>
2022-12-07 18:35:54 +0100

    added env

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const fs = require("fs");
+require("dotenv").config();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.log(req.app.get("env"));
-  res.render("index", { title: "Lorenzo", dev: "prod" });
+  res.render("index", { title: "Lorenzo", dev: process.env.MODE });
 });
 
 module.exports = router;
