a35afadabfe0cbddeddb8ae8d14847fe5359b0b7
lolo298 <lorenzoaoulini@gmail.com>
2022-12-28 17:12:35 +0100

    Robots.txt

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use('/robots.txt', function (req, res, next) {
+  res.type('text/plain')
+  res.send("User-agent: *\nDisallow: /");
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 /* GET home page. */
-router.get('/robots.txt', function (req, res, next) {
-  res.type('text/plain');
-  res.send("User-agent: *\nDisallow: /");
-});
+
 router.get("/", function (req, res, next) {
   console.log(req.app.get("env"));
   res.render("index", { title: "Lorenzo Aoulini | Portfolio", dev: process.env.MODE });
