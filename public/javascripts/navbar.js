793b1e3106434c88987a334a6df24d1afc0ee7f7
lolo298 <lorenzoaoulini@gmail.com>
2023-01-30 16:23:22 +0100

    Missing elements from navbar

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -2,15 +2,21 @@ var projects, timeline, contact;
 projects = document.createElement("p");
 projects.innerHTML = lang == "en" ? "Projects" : "Projets";
 projects.classList.add("navItem");
-projects.addEventListener("click", function () {
-  window.location.href = "/projects";
+projects.addEventListener("mousedown", function (e) {
+  e.preventDefault();
+  console.log(e);
+  if (e.which == 1) window.location.href = "/projects";
+  else window.open("/projects", "_blank");
 });
 
 timeline = document.createElement("p");
 timeline.innerHTML = "Experience";
 timeline.classList.add("navItem");
-timeline.addEventListener("click", function () {
-  window.location.href = "/timeline";
+timeline.addEventListener("mousedown", function (e) {
+  e.preventDefault();
+  console.log(e);
+  if (e.which == 1) window.location.href = "/timeline";
+  else window.open("/timeline", "_blank");
 });
 
 contact = document.createElement("p");
